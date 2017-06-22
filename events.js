1dc78105ae52f5d50fd268b1430c296f09120229
Usha <usha.macdonald@gmail.com>
2017-06-22 18:15:04 +1200

    added function three

--- events.js
+++ events.js
@@ -8,6 +8,7 @@ function start () {
   // when you mouse over it.
   one()
   two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -31,6 +32,11 @@ function two () {
 }
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener('mouseleave', makeWhite)
+}
 
 // CREATE FUNCTION four HERE
 
@@ -44,4 +50,7 @@ function makeWhite (evt) {
 }
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green'
+}
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
