7bac5e88dbad1632fbdccc1b4a7c73bb909a5953
Usha <usha.macdonald@gmail.com>
2017-06-22 18:18:20 +1200

    added function four

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -39,6 +40,10 @@ function three () {
 }
 
 // CREATE FUNCTION four HERE
+function four() {
+  var four = document.getElementById('four')
+  four.addEventListener('click', makeRed)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -53,4 +58,7 @@ function makeGreen (evt) {
 }
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange'
+}
+function makeRed (evt) {
+  evt.target.style.backgroundColor = 'red'
 }
